58dad61e39600d365843bf41478b944b2c4a4b77
Gurjaipal17 <163448707+Gurjaipal17@users.noreply.github.com>
2024-07-19 01:28:54 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -6,8 +6,7 @@ export const StoreContext=createContext(null)
 const StoreContextProvider=(props)=>{
 
     const [cartItems,setCartItems]=useState({});
-    const url="https://food-backend-imml.onrender.com";///
-    // const url="https://food-backend-imml.onrender.com";///
+    const url="https://fooddel-backend-qlgs.onrender.com";///
     const [token,setToken]=useState("")
     const [food_list,setFoodList]=useState([])
 
