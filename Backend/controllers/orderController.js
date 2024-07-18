2f5b5f08a92cb2fdac1ac514b4985e3cd4a9d2d3
Gurjaipal17 <163448707+Gurjaipal17@users.noreply.github.com>
2024-07-19 01:41:37 +0530

    Update orderController.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order for frontend
 const placeOrder=async(req,res)=>{
 
-    const frontend_url="https://fooddelivery-frontend-rnzs.onrender.com/";
+    const frontend_url="https://fooddelivery-frontend-rnzs.onrender.com";
     //Change here ig u want to change port
 
     try {
