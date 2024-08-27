89f63d789bc749f844476da9a093e0f4fed98c3a
Gurjaipal Singh <163448707+Gurjaipal17@users.noreply.github.com>
2024-08-28 00:29:53 +0530

    Update Cart.jsx

--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -30,9 +30,9 @@ const navigate = useNavigate();
                 <div className='cart-items-title cart-items-item'>
                   <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>${item.price}</p>
+                  <p> &#x20B9;{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>${item.price*cartItems[item._id]}</p>
+                  <p> &#x20B9;{item.price*cartItems[item._id]}</p>
                   <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
                   </div>
                   <hr/></div>
@@ -46,29 +46,20 @@ const navigate = useNavigate();
           <div>
             <div className='cart-total-details'>
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p> &#x20B9;{getTotalCartAmount()}</p>
             </div>
             <hr/>
             <div className='cart-total-details'>
-              <p>Delivery Fee</p>
-              <p>{getTotalCartAmount()===0?0:2}</p>
+              <p>Packaging Fee</p>
+              <p> &#x20B9;{getTotalCartAmount()===0?0:2}</p>
             </div>
             <hr/>
             <div className='cart-total-details'>
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              <b> &#x20B9;{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
             </div>
           </div>
             <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
-        </div>
-        <div className="cart-promocode">
-          <div>
-            <p>If you have a promo code, Enter it here</p>
-            <div className='cart-promocode-input'>
-              <input type="text" placeholder='promo code'/>
-              <button>Submit</button>
-            </div>
-          </div>
         </div>
       </div>
     </div>
