77b4c5a13a9f2ceacc73a8906cc5b985274a6fe1
Gurjaipal Singh <163448707+Gurjaipal17@users.noreply.github.com>
2024-08-28 01:03:30 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -50,7 +50,7 @@ function List({url}) {
         <img src={`${url}/images/`+item.image} alt="" />
         <p>{item.name}</p>
         <p>{item.category}</p>
-        <p>${item.price}</p>
+        <p>&#x20B9;{item.price}</p>
         <p onClick={()=>removeFood(item._id)} className='cursor'>X</p>
       </div>
 
