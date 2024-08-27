6eb8f8f8d99336f26b3a2f96a80aeb2271136860
Gurjaipal Singh <163448707+Gurjaipal17@users.noreply.github.com>
2024-08-27 23:12:16 +0530

    Update Navbar.jsx

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -52,8 +52,7 @@ const Navbar = ({ setShowLogin }) => {
         </a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
-        <div className="navbar-search-icon">
+               <div className="navbar-search-icon">
           <Link to="/cart">
             <img src={assets.basket_icon} alt="" />
           </Link>
